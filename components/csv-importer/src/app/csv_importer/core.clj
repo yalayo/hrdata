6ea483afe361b37be5cad8ff136469f14c68c9de
Yunior Alayo <yunior.alayo@gmail.com>
2024-10-04 21:53:46 +0200

    Added code to process a csv file.

--- components/csv-importer/src/app/csv_importer/core.clj
+++ components/csv-importer/src/app/csv_importer/core.clj
@@ -1,1 +1,7 @@
-(ns app.csv-importer.core)
+(ns app.csv-importer.core
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
+
+(defn process-file [input-stream]
+  (with-open [reader (io/reader input-stream)]
+    (csv/read-csv reader)))
--- components/csv-importer/src/app/csv_importer/interface.clj
+++ components/csv-importer/src/app/csv_importer/interface.clj
@@ -1,2 +1,5 @@
 (ns app.csv-importer.interface
   (:require [app.csv-importer.core :as core]))
+
+(defn process-file [input-stream]
+  (core/process-file input-stream))
