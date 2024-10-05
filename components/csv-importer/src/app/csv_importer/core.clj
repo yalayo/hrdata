78e1ad9d164c3e9cb90bc7511a1878bebec8d2df
Yunior Alayo <yunior.alayo@gmail.com>
2024-10-05 12:14:59 +0200

    Return a better formated data.

--- components/csv-importer/src/app/csv_importer/core.clj
+++ components/csv-importer/src/app/csv_importer/core.clj
@@ -2,6 +2,11 @@
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]))
 
+(defn vectors-to-maps [data]
+  (let [keys (mapv keyword (first data))
+        values (rest data)]
+    (mapv #(zipmap keys %) values)))
+
 (defn process-file [input-stream]
   (with-open [reader (io/reader input-stream)]
-    (csv/read-csv reader)))
+    (vectors-to-maps (csv/read-csv reader))))
